f491bc05168470f8498f29c1cebf349b614ad51b
santtone <sami.anttonen@trimble.com>
2019-05-28 00:17:58 +0300

    Repository test using in-memory db. Moq tests.

---
+++ ContactsWebApiTest/ContactControllerMoqTest.cs
@@ -0,0 +1,36 @@
+﻿using ContactsWebApi.Controllers;
+using ContactsWebApi.Models;
+using ContactsWebApi.Services;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace ContactsWebApiTest
+{
+    public class ContactControllerMoqTest
+    {
+        [Fact]
+        public async void GetShouldReturnSpecifiedContact()
+        {
+            //Arrange
+            const int id = 1;
+            var mockContactService = new Mock<IContactService>();
+            mockContactService.Setup(contactService => contactService.Get(id))
+                .ReturnsAsync(new Contact
+                {
+                    Id = id,
+                    FirstName = "Hei",
+                    LastName = "Moi"
+                });
+            var contactController = new ContactController(mockContactService.Object);
+
+            //Act
+            var action = await contactController.Get(id);
+
+            //Assert
+            var result = Assert.IsType<JsonResult>(action);
+            var contact = Assert.IsType<Contact>(result.Value);
+            Assert.Equal(id, contact.Id);
+        }
+    }
+}
---
+++ ContactsWebApiTest/ContactRepositoryTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ContactsWebApi.Config;
+using ContactsWebApi.Models;
+using ContactsWebApi.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace ContactsWebApiTest
+{
+    public class ContactRepositoryTest
+    {
+        public ContactRepositoryTest()
+        {
+        }
+
+        private static ContactsDbContext GetDbContext()
+        {
+            var options = new DbContextOptionsBuilder<ContactsDbContext>()
+                .UseInMemoryDatabase("TestDB")
+                .Options;
+
+            var context = new ContactsDbContext(options);
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            context.AddRange(new List<Contact>
+            {
+                new Contact {FirstName = "ContactFirstName", LastName = "ContactLastName"},
+                new Contact {FirstName = "OtherContactFirstName", LastName = "OtherContactLastName"}
+            });
+            context.SaveChanges();
+            return context;
+        }
+
+        [Fact]
+        private async void GetShouldReturnAllContacts()
+        {
+            //Arrange
+            var context = GetDbContext();
+            IContactRepository contactRepository = new ContactRepository(context);
+
+            //Act
+            var contacts = await contactRepository.Get();
+
+            //Assert
+            Assert.Equal(contacts.Count, context.Contact.ToList().Count);
+        }
+    }
+}
