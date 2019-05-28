ba6fbcfc4a631f5e5ca234bff124f35a3336b1fc
santtone <sami.anttonen@trimble.com>
2019-05-28 12:26:30 +0300

    Added xUnit Project

---
+++ ContactsWebApiTest/ContactControllerTest.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+
+namespace ContactsWebApiTest
+{
+    public class ContactControllerTest
+    {
+        [Fact]
+        public void TestSomething()
+        {
+            Assert.Equal(1,1);
+        }
+
+        public void DoSomething()
+        {
+
+        }
+    }
+}
